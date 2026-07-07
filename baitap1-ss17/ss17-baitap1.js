356240612b03448dd0e2501d2c8ddfc7a19f392b
ngominhquyen1309-sys <ngominhquyen1309@gmail.com>
2026-07-07 16:58:15 +0900

    NOP BAI

---
+++ baitap1-ss17/ss17-baitap1.js
@@ -0,0 +1,7 @@
+document.getElementById('hide-text').addEventListener('click', function() {
+    document.getElementById('text').style.display = 'none';
+});
+
+document.getElementById('show-text').addEventListener('click', function() {
+    document.getElementById('text').style.display = 'block';
+});
